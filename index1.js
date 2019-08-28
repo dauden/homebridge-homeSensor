5024a23b6abcf2ab53764109fdb2769f56c0881e
0xbizon <tuananh@autonomous.nyc>
2019-08-28 13:13:04 +0700

    update

--- index1.js
+++ index1.js
@@ -1,6 +1,6 @@
 var sensor = require('node-dht-sensor').promises;
  
-sensor.read(22, 17).then(
+sensor.read(11, 17).then(
     function ({ temperature, humidity }) {
       console.log('temp: ' + temperature.toFixed(1) + '°C, ' +
           'humidity: ' + humidity.toFixed(1) + '%'
